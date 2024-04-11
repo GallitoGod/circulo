3d6b94d6f80d9c2b6425277bcd97690c1f9b11e9
Gallo Pablo <gallopabloj@gmail.com>
2024-04-11 19:35:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+
+
+function App() {
+
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    console.log('effect: ',{enabled});
+  }, [enabled])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: 'traslate(0px, 0px)'
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}> 
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
+  )
+}
+
+export default App
